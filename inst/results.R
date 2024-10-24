143ca08133b44ebc2351f047cf3749a9eb17bb46
pchang27 <pchang27@ufl.edu>
2024-10-24 16:02:41 -0400

    small adjustment

--- inst/results.R
+++ inst/results.R
@@ -1,9 +1,11 @@
-#Simulation Case 1 (4.1)
+#Simulation Case 1 (Section 4.1)
 
 source("rbf_4.1_mse.R")
 
-#Simulation Case 2 (4.2)
+#Simulation Case 2 (Section 4.2)
 
 source("rbf_4.2_mse.R")
 source("rbf_4.2_blp.R")
 source("rbf_4.2_threshold.R")
+
+#MIMIC data (Section 5)
